a8f7d07f6a552499a316d1556c8911bd359ecf34
Winter Child <wintertideheir@gmail.com>
2017-04-08 21:49:35 -0400

    Add OpenGL shader code to desktop executable

--- src/desktop/Main.hs
+++ src/desktop/Main.hs
@@ -4,8 +4,84 @@ import Control.Monad
 import System.IO.Error
 import Graphics.UI.GLFW as GLFW
 import Graphics.GL.Core33
+import Foreign.Marshal
+import Foreign.C.String
+import Foreign.Storable
+import Foreign.Ptr
 
-begin :: IO Window
+data AikaterineState = AikaterineState
+  { windowState     :: Window
+  , programState    :: GLuint }
+
+createShader :: String -> GLenum -> IO GLuint
+createShader source shaderType = do
+  shader <- glCreateShader shaderType
+  withCString source (\source ->
+    with source (\sources ->
+      glShaderSource shader 1 sources nullPtr))
+  glCompileShader shader
+  success <-
+    alloca (\success -> do
+      glGetShaderiv shader GL_COMPILE_STATUS success
+      peek (castPtr success))
+  when (success == GL_FALSE) (do
+    let
+      userErrorMessage =
+        case shaderType of
+          GL_VERTEX_SHADER ->   "Vertex shader failed to compile."
+          GL_FRAGMENT_SHADER -> "Fragment shader failed to compile."
+          otherwise ->          "Shader failed to compile."
+    allocaArray 512 (\info -> do
+      glGetShaderInfoLog shader 512 nullPtr info
+      info <- peekCString info
+      ioError (userError (userErrorMessage ++
+                          (if (info == "") then "" else " ") ++ info))))
+  return shader
+
+shaderVertex :: IO GLuint
+shaderVertex = createShader
+  "#version 330 core\n\
+  \layout (location = 0) in vec3 position;\n\
+  \void main()\n\
+  \{\n\
+  \gl_Position = vec4(position, 1.0);\n\
+  \}\n"
+  GL_VERTEX_SHADER
+
+shaderFragment :: IO GLuint
+shaderFragment = createShader
+  "#version 330 core\n\
+  \out vec4 color;\n\
+  \void main()\n\
+  \{\n\
+  \color = vec4(0.5f, 0.5f, 0.5f, 1.0f);\n\
+  \}\n"
+  GL_FRAGMENT_SHADER
+
+shaderProgram :: IO GLuint
+shaderProgram = do
+  program <- glCreateProgram
+  vertex <- shaderVertex
+  fragment <- shaderFragment
+  glAttachShader program vertex
+  glAttachShader program fragment
+  glLinkProgram program
+  success <- alloca (\success -> do
+    glGetProgramiv program GL_LINK_STATUS success
+    peek (castPtr success))
+  when (success == GL_FALSE) (do
+    info <- (allocaArray 512 (\info -> do
+      glGetProgramInfoLog program 512 nullPtr info
+      peekCString info))
+    ioError (userError ("Program failed linking.\n" ++
+             (if (info == "") then "" else " " ++ info))))
+  glDetachShader program vertex
+  glDeleteShader vertex
+  glDetachShader program fragment
+  glDeleteShader fragment
+  return program
+
+begin :: IO AikaterineState
 begin = do
   glfwInitSuccess <- GLFW.init
   when (not glfwInitSuccess) (ioError (userError "GLFW failed to initialize."))
@@ -20,25 +96,26 @@ begin = do
       windowSize <- getFramebufferSize window
       let toInt32 x = fromInteger $ toInteger x in
         glViewport 0 0 (toInt32 $ fst windowSize) (toInt32 $ snd windowSize)
-      return window
+      program <- shaderProgram
+      return (AikaterineState window program)
     Nothing -> ioError (userError "GLFW failed to create new window.")
 
-loop :: Window -> IO ()
-loop window = do
-  closeRequest <- windowShouldClose window
+loop :: AikaterineState -> IO ()
+loop aS = do
+  closeRequest <- windowShouldClose (windowState aS)
   if closeRequest
     then return ()
     else do
       pollEvents
-      swapBuffers window
+      swapBuffers (windowState aS)
       glClear GL_COLOR_BUFFER_BIT
-      loop window
+      loop aS
 
 end :: IO ()
 end = terminate
 
 main :: IO ()
 main = do
-  window <- begin
-  loop window
+  aS <- begin
+  loop aS
   end
