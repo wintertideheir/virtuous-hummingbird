0032b3eba67bae8453222d310d9ef937eb49a6d2
Winter Child <wintertideheir@gmail.com>
2017-04-10 21:41:57 -0400

    Add type Region and function regionsFromNetwork
    Add type Region to describe regions of a knowledge network and a
    function regionsFromNetwork to map the names of regions to lists of
    Ideas.

--- src/Aikaterine.hs
+++ src/Aikaterine.hs
@@ -1,6 +1,7 @@
 module Aikaterine where
 
 import Data.Graph.Inductive
+import Data.Map
 
 data Idea n = Idea { region     :: String
                    , name       :: String
@@ -10,3 +11,17 @@ data Idea n = Idea { region     :: String
 newtype Relation = Relation String
 
 newtype KnowledgeNetwork n = KnowledgeNetwork (Gr (Idea n) Relation)
+
+data Region n = Region String [Idea n] [Region n]
+
+regionsFromNetwork :: KnowledgeNetwork n -> Map String [Idea n]
+regionsFromNetwork (KnowledgeNetwork kn) =
+  getRegions (Prelude.map snd (labNodes kn)) Data.Map.empty
+    where
+      updateRegion i mis =
+        case mis of
+          Just is -> Just (i:is)
+          Nothing -> Nothing
+      getRegions [] rm = rm
+      getRegions (i:is) rm =
+        getRegions is (alter (updateRegion i) (region i) rm)
