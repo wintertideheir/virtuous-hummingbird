a71f7faae49cbdfc5007fc9dc7e3123a05952571
Winter Child <wintertideheir@gmail.com>
2017-07-23 20:57:44 -0400

    Copy function argument names from the implementation to the header

--- src/aikaterine.h
+++ src/aikaterine.h
@@ -13,8 +13,8 @@ struct Rectangle {
 typedef struct KnowledgeGraph KnowledgeGraph;
 
 KnowledgeGraph* knowledge_graph_new();
-void knowledge_graph_add(KnowledgeGraph*, int, char*, struct Position);
-void knowledge_graph_remove(KnowledgeGraph*, int);
-void knowledge_graph_connect(KnowledgeGraph*, int, int, int);
-void knowledge_graph_disconnect(KnowledgeGraph*, int, int, int);
+void knowledge_graph_add(KnowledgeGraph* kn, int region, char* idea, struct Position pos);
+void knowledge_graph_remove(KnowledgeGraph* kn, int vertex);
+void knowledge_graph_connect(KnowledgeGraph* kn, int from, int to, int relation);
+void knowledge_graph_disconnect(KnowledgeGraph* kn, int from, int to, int relation);
 void knowledge_graph_free(KnowledgeGraph*);
