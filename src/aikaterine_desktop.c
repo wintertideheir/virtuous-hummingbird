9c79d6ae175bd21e2318f31085945320be0a76ce
Winter Child <wintertideheir@gmail.com>
2017-07-24 16:21:54 -0400

    Change vertex shader input for 2D points

--- src/aikaterine_desktop.c
+++ src/aikaterine_desktop.c
@@ -95,13 +95,13 @@ int main(int argc, char const *argv[])
 
   const GLchar* vertexShaderCode =
     "#version 330 core\n"
-    "layout (location = 0) in vec3 pos;\n"
+    "layout (location = 0) in vec2 pos;\n"
     "uniform float scale;\n"
     "uniform int windowX;\n"
     "uniform int windowY;\n"
     "void main()\n"
     "{\n"
-    "    gl_Position = vec4(5 * scale * pos.xy / vec2(windowX, windowY), pos.z, 1.0);\n"
+    "    gl_Position = vec4(5 * scale * pos.xy / vec2(windowX, windowY), 0.0, 1.0);\n"
     "}\n";
 
   const GLchar* fragmentShaderCode =
@@ -153,12 +153,12 @@ int main(int argc, char const *argv[])
   glDeleteShader(fragmentShader);
 
   float vertices[] = {
-     0.5f,  0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-    -0.5f,  0.5f, 0.0f,
-     0.5f, -0.5f, 0.0f,
-    -0.5f, -0.5f, 0.0f,
-    -0.5f,  0.5f, 0.0f
+     0.5f,  0.5f,
+     0.5f, -0.5f,
+    -0.5f,  0.5f,
+     0.5f, -0.5f,
+    -0.5f, -0.5f,
+    -0.5f,  0.5f,
   };
 
   unsigned int VBO;
@@ -170,7 +170,7 @@ int main(int argc, char const *argv[])
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
