4cf13048b0db7dd2b9abadafe66346c4c1f5d465
Winter Child <wintertideheir@gmail.com>
2017-07-24 15:01:36 -0400

    Add comments to the library

--- src/aikaterine.h
+++ src/aikaterine.h
@@ -8,6 +8,8 @@ struct Position {
 struct Rectangle {
   struct Position center;
   struct Position offset;
+    /* The offset is treated as a vector, whose attributes should never be less
+       than zero. */
 };
 
 typedef struct KnowledgeGraph KnowledgeGraph;
@@ -16,5 +18,6 @@ KnowledgeGraph* knowledge_graph_new();
 void knowledge_graph_add(KnowledgeGraph* kn, int region, char* idea, struct Position pos);
 void knowledge_graph_remove(KnowledgeGraph* kn, int vertex);
 void knowledge_graph_connect(KnowledgeGraph* kn, int from, int to, int relation);
+  // Undefined behavior if the edge already exists.
 void knowledge_graph_disconnect(KnowledgeGraph* kn, int from, int to, int relation);
 void knowledge_graph_free(KnowledgeGraph*);
