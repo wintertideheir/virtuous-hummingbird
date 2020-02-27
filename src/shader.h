f5c00a32f766a09f603822586da5411b228f4854
Sagun Singh <sagun.singh.vt@gmail.com>
2020-02-27 07:58:38 -0500

    Remove `createShader` declaration from `shader.h`

--- src/shader.h
+++ src/shader.h
@@ -7,6 +7,5 @@ struct ShaderRequest {
   int indicies_len;
 };
 
-GLuint createShader(GLenum type, GLsizei number, const GLchar **code);
 GLuint createProgram(const GLchar** shader_code,
                      struct ShaderRequest* shader_req, int shader_req_len);
