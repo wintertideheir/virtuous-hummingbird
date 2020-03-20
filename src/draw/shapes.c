d8a0db3c104e4e8df2a5bed335d8c783f210f478
Sagun Singh <sagun.singh.vt@gmail.com>
2020-03-20 18:46:45 -0400

    Unify the type signature of `shapesGenerateBox`

--- src/draw/shapes.c
+++ src/draw/shapes.c
@@ -34,7 +34,7 @@ void shapesBegin()
                                                               .code = (const GLchar*[]){boxFragmentShaderCode}}});
 }
 
-void shapesGenerateBox(float lower_x, float upper_x, float lower_y, float upper_y,
+void shapesGenerateBox(float upper_x, float lower_x, float upper_y, float lower_y,
                        float layer, struct RGBA color,
                        unsigned int *VAO, unsigned int *VBO)
 {
