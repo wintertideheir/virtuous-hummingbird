b928728078ff99da19a90e60a076306b5134ff46
Sagun Singh <sagun.singh.vt@gmail.com>
2020-07-22 09:33:08 -0400

    Change app to use TabLayout for navigation
    Remove the Snackbar button and add a TabLayout for navigation. Write a
    TabSelector class (implementing TabLayout.OnTabSelectedListener) to
    control navigation.
    
    Clean up autogenerated comments in code and xml files.
    
    Clean up fragments by removing buttons and aligning text.

---
+++ app/src/main/java/net/wintertideheir/aikaterine/TabSelector.kt
@@ -0,0 +1,20 @@
+package net.wintertideheir.aikaterine
+
+import androidx.navigation.NavController
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayout.Tab
+
+class TabSelector(val navController: NavController) : TabLayout.OnTabSelectedListener {
+
+    override fun onTabReselected(p0: Tab) { }
+
+    override fun onTabUnselected(p0: Tab) { }
+
+    override fun onTabSelected(p0: Tab) {
+        when (p0.contentDescription.toString()) {
+            "tab1" -> navController.navigate(R.id.action_SecondFragment_to_FirstFragment)
+            "tab2" -> navController.navigate(R.id.action_FirstFragment_to_SecondFragment)
+        }
+    }
+
+}
