3274f2f04685436e1be3443732e19ef30d1f3991
Sagun Singh <sagun.singh.vt@gmail.com>
2020-07-22 23:17:23 -0400

    Fix FragmentAnalysis inflating the wrong layout

--- app/src/main/java/net/wintertideheir/aikaterine/FragmentAnalysis.kt
+++ app/src/main/java/net/wintertideheir/aikaterine/FragmentAnalysis.kt
@@ -12,7 +12,7 @@ class FragmentAnalysis : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_tracker, container, false)
+        return inflater.inflate(R.layout.fragment_analysis, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
