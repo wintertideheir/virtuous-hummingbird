b179ee5c0d688eec9961bf70b4e3679a4cc9a862
Sagun Singh <sagun.singh.vt@gmail.com>
2020-07-26 06:43:38 -0400

    Remove the trait change memento interface
    The memento and command pattern classes were originally conceived as
    part of a history tracking system for undo/redo functionality. It's
    become clear that they are unnecessary.

--- AikaterineLibrary/src/main/java/net/wintertideheir/aikaterine/TraitMemento.kt
+++
@@ -1,9 +0,0 @@
-package net.wintertideheir.aikaterine
-
-import java.io.Serializable
-
-interface TraitMemento : Serializable {
-
-    fun apply(traitTree: net.wintertideheir.aikaterine.TraitTree)
-
-}
