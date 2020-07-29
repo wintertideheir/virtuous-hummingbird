67da794a873be373358d9210a9dd5b06b830b347
Sagun Singh <sagun.singh.vt@gmail.com>
2020-07-29 12:47:29 -0400

    Clean up Trait class
    Change the Trait data class to a regular class to allow direct
    comparisons by references.
    
    Refactor the `cyclic` function of the Trait function to be shorter.

--- AikaterineLibrary/src/main/java/net/wintertideheir/aikaterine/Trait.kt
+++ AikaterineLibrary/src/main/java/net/wintertideheir/aikaterine/Trait.kt
@@ -2,22 +2,17 @@ package net.wintertideheir.aikaterine
 
 import java.io.Serializable
 
-data class Trait(var shortDescription: String,
-                 var longDescription:  String,
-                 var parent: Trait?,
-                 var children: MutableList<Trait>) : Serializable {
+class Trait(var shortDescription: String,
+            var longDescription:  String,
+            var parent: Trait?,
+            var children: MutableList<Trait>) : Serializable {
 
     fun cyclic(visited: MutableList<Trait> = arrayListOf()): Boolean {
         if (this in visited) {
             return true
         }
         visited.add(this)
-        for (child in children) {
-            if (child.cyclic(visited)) {
-                return true
-            }
-        }
-        return false
+        return (true in children.map { it.cyclic(visited) })
     }
 
 }
