0218735bb030cb865c3b59b65cc07f3c7cf5b632
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-12 10:08:43 -0400

    Write JavaFX main application class

--- src/main/java/com/autumnara/aikaterine/App.java
+++ src/main/java/com/autumnara/aikaterine/App.java
@@ -1,7 +1,23 @@
 package com.autumnara.aikaterine;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application{
+
+    @Override
+    public void start(Stage stage) {
+        Label l = new Label("Aikaterine");
+        Scene scene = new Scene(new StackPane(l), 640, 480);
+        stage.setScene(scene);
+        stage.show();
     }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
