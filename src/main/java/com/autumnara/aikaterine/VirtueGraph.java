45efb559404d137a71717b5ffd710ed6ffc39b00
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-21 10:22:00 -0400

    Add a final root node to the virtue graph

--- src/main/java/com/autumnara/aikaterine/VirtueGraph.java
+++ src/main/java/com/autumnara/aikaterine/VirtueGraph.java
@@ -7,9 +7,13 @@ import org.jgrapht.graph.DefaultEdge;
 
 public class VirtueGraph extends DirectedAcyclicGraph<Virtue, DefaultEdge> {
 
+    public final Virtue root;
+
     public VirtueGraph()
     {
         super(DefaultEdge.class);
+        this.root = new Virtue("εὐδαιμονία", "Flourishing, prosperity, or blessedness");
+        this.addVertex(this.root);
     }
 
 }
