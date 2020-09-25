a561a8b1a666861810fe90842ad83ca154fc16c6
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-25 04:46:18 -0400

    Add documentation and EOF newline to VirtueGraph

--- src/main/java/com/autumnara/aikaterine/VirtueGraph.java
+++ src/main/java/com/autumnara/aikaterine/VirtueGraph.java
@@ -5,10 +5,17 @@ import com.autumnara.aikaterine.Virtue;
 import org.jgrapht.graph.DirectedAcyclicGraph;
 import org.jgrapht.graph.DefaultEdge;
 
+/** A {@link org.jgrapht.graph.DirectedAcyclicGraph} of 
+    {@link com.autumnara.aikaterine.Virtue} with relevant auxiliary
+    functions. */
 public class VirtueGraph extends DirectedAcyclicGraph<Virtue, DefaultEdge> {
 
+    /** The highest virtue, εὐδαιμονία, from which all connected edges
+        should be directed from. */
     public final Virtue root;
 
+    /** Constructor for a virtue graph. The graph is initially
+        populated with the root virtue. */
     public VirtueGraph()
     {
         super(DefaultEdge.class);
