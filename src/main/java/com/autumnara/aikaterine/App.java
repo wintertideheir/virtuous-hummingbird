7072a3e97d5ccf83194a2184ec69bbc7959f8dc2
Sagun Singh <sagun.singh.vt@gmail.com>
2020-09-28 05:34:06 -0400

    Remove JavaFX
    This commit completely removes JavaFX. The JVM platform and the Java
    language ought to be retained, but a better interface could be
    developed from OpenGL, like the earliest versions of Aikaterine.

--- src/main/java/com/autumnara/aikaterine/App.java
+++ src/main/java/com/autumnara/aikaterine/App.java
@@ -1,31 +1,9 @@
 package com.autumnara.aikaterine;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Accordion;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-import java.net.URL;
-
-public class App extends Application{
-
-    @Override
-    public void start(Stage stage)
-    throws IOException {
-        URL fxml = getClass().getResource("/aikaterine.fxml");
-        FXMLLoader fxmlLoader = new FXMLLoader(fxml);
-        Accordion root = (Accordion) fxmlLoader.load();
-        Scene scene = new Scene(root);
-
-        stage.setScene(scene);
-        stage.setTitle("Aikaterine");
-        stage.show();
-    }
+public class App {
 
     public static void main(String[] args) {
-        launch();
+        System.out.println("Hello world!");
     }
 
 }
