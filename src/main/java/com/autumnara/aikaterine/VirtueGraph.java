71745b2276f11341dddfe51e5ab459987b3415f3
Sagun Singh <wintertideheir@users.noreply.github.com>
2020-11-24 20:26:33 -0500

    Add a virtue graph focus
    Add a virtue graph focus member to keep track of which virtue is being
    edited or viewed.

--- src/main/java/com/autumnara/aikaterine/VirtueGraph.java
+++ src/main/java/com/autumnara/aikaterine/VirtueGraph.java
@@ -14,6 +14,11 @@ public class VirtueGraph extends DirectedAcyclicGraph<Virtue, DefaultEdge> {
         should be directed from. */
     public final Virtue root;
 
+    /** The virtue presently being edited or viewed. The view of the
+        graph is centered over this virtue and oriented so that this
+        virtue is horizontal and above the root node. */
+    private Virtue focus;
+
     /** Constructor for a virtue graph. The graph is initially
         populated with the root virtue. */
     public VirtueGraph()
@@ -21,6 +26,7 @@ public class VirtueGraph extends DirectedAcyclicGraph<Virtue, DefaultEdge> {
         super(DefaultEdge.class);
         this.root = new Virtue("εὐδαιμονία", "Flourishing, prosperity, or blessedness");
         this.addVertex(this.root);
+        this.focus = this.root;
     }
 
 }
