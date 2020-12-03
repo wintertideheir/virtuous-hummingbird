0edfff364527f5b0f3485857257ad98b6170ae4f
Sagun Singh <wintertideheir@users.noreply.github.com>
2020-12-03 14:08:27 -0500

    Add common angles as constants
    Add a constant for the null angle, 0, the angle of the polar axis, to
    the polar position class.
    
    Add a constant for the angle of a complete revolution, 2 * PI, to the
    polar position class.

--- src/main/java/com/autumnara/aikaterine/PositionPolar.java
+++ src/main/java/com/autumnara/aikaterine/PositionPolar.java
@@ -1,8 +1,15 @@
 package com.autumnara.aikaterine;
 
+import static java.lang.Math.*;
+
 /** A position expressed in polar coordinates. */
 public final class PositionPolar
 {
+    /** The angle of the polar axis. */
+    public final static float NULL_ANGLE = 0;
+
+    /** The angle of a complete revolution about the pole. */
+    public final static float REVOLUTION_ANGLE = 2 * (float) PI;
 
     /** The polar angle of this position relative to the polar axis. */
     public final float angle;
