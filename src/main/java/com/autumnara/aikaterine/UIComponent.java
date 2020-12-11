5ab9004b306ffc458e2f66353574cb7113fece40
Sagun Singh <wintertideheir@users.noreply.github.com>
2020-12-11 14:53:33 -0500

    Change UIComponent to an interface

--- src/main/java/com/autumnara/aikaterine/UIComponent.java
+++ src/main/java/com/autumnara/aikaterine/UIComponent.java
@@ -1,20 +1,20 @@
 package com.autumnara.aikaterine;
 
 /** A component that can be drawn at specific positions. */
-public abstract class UIComponent
+public interface UIComponent
 {
 
     /** Make this component drawable. */
-    public abstract void initialize();
+    public void initialize();
     
     /** Draw this component with it's present configuration. */
-    public abstract void draw();
+    public void draw();
 
     /** Set the allocated drawing space by OpenGL window coordinates. */
-    public abstract void setDrawingArea(float xMin, float yMin,
-                                        float xMax, float yMax);
+    public void setDrawingArea(float xMin, float yMin,
+                               float xMax, float yMax);
 
     /** Make this component undrawable and free all of it's resources. */
-    public abstract void terminate();
+    public void terminate();
 
 }
