7810402ff976a23c44578296c60d121fd71a60ac
Sagun Singh <wintertideheir@users.noreply.github.com>
2020-12-22 12:08:57 -0500

    Revert "Split UIComponent"
    This reverts commit 5f3e2452c3161027d17da1327581622ea38f7120.
    
    The above commit was intended to allow a new window class and component
    classes to share code. That change, however, would have allowed a
    component to be initialized standalone with respect to a window, which
    we do not want. Furthermore, this situation violates an important
    principle of inheretance, which states that classes must be replacable
    in functionality by their subclasses.

--- src/main/java/com/autumnara/aikaterine/UIComponent.java
+++ src/main/java/com/autumnara/aikaterine/UIComponent.java
@@ -5,8 +5,11 @@ import static java.lang.Math.*;
 /** A component that can be drawn at specific positions. By default
     this component does nothing and must be extended for any
     functionality. */
-public class UIComponent extends UIObject
+public class UIComponent
 {
+
+    /** Whether this component is drawable or not. */
+    private boolean drawable = false;
 
     /** The lower bound of this component in OpenGL clip space.
         Must be between -1f and 1f, and less than {@link #xMax}. */
@@ -24,11 +27,48 @@ public class UIComponent extends UIObject
         Must be between -1f and 1f, and greater than {@link #yMin}. */
     protected float yMax;
 
+    /** Make this component drawable.
+        This method performs basic error checking and then calls
+        {@link #onInitialize}. This method must be called before any
+        other method. Without calling {@link #setDrawingArea} after,
+        drawing this component will draw to the default boundaries (the
+        entire window). */
+    public final void initialize()
+    {
+        if (this.drawable)
+        {
+            throw new IllegalStateException("Component to be initialized is already initialized.");
+        }
+        this.onInitialize();
+        this.drawable = true;
+    }
+
+    /** Make this component drawable.
+        By default this method does nothing and can be overrided by
+        subclasses. */
+    protected void onInitialize() {}
+
+    /** Draw this component with it's present configuration.
+        This method checks that this component is drawable and then
+        calls {@link #onDraw}. */
+    public final void draw()
+    {
+        if (!this.drawable)
+        {
+            throw new IllegalStateException("Component needs to be initialized before being drawn.");
+        }
+        this.onDraw();
+    }
+
+    /** Draw this component with it's present configuration.
+        By default this method does nothing and can be overrided by
+        subclasses. Drawing should occur in the boundaries set by
+        {@link #setDrawingArea}. */
+    protected void onDraw() {}
+
     /** Set the allocated drawing space by OpenGL window coordinates.
         This method will call {@link #onSetDrawingArea} after setting
-        the drawing area. Drawing through {@link #draw} /
-        {@link #onDraw} should occur in the boundaries set by
-        {@link #setDrawingArea}. The component needs to be drawable.
+        the drawing area.
         @param xMin The lower bound of this component in OpenGL clip
                     space. Must be between -1f and 1f, and less than
                     xMax.
@@ -44,7 +84,7 @@ public class UIComponent extends UIObject
     public final void setDrawingArea(float xMin, float yMin,
                                      float xMax, float yMax)
     {
-        if (!this.drawable())
+        if (!this.drawable)
         {
             throw new IllegalStateException("Component needs to be initialized before the drawing area can be set.");
         }
@@ -78,5 +118,24 @@ public class UIComponent extends UIObject
         By default this method does nothing and can be overrided by
         subclasses. */
     protected void onSetDrawingArea() {}
+
+    /** Make this component undrawable and free all of it's resources.
+        This method calls {@link #onTerminate} and marks this component
+            as undrawable. */
+    public final void terminate()
+    {
+        if (!this.drawable)
+        {
+            throw new IllegalStateException("Component being terminated is not drawable.");
+        }
+
+        this.onTerminate();
+        this.drawable = false;
+    }
+
+    /** Make this component undrawable and free all of it's resources.
+        By default this method does snothing and can be overrided by
+        subclasses. */
+    protected void onTerminate() {}
 
 }
