b7a7e0ee0790c0c11e5439ffeabe711c2e27f5f0
Sagun Singh <wintertideheir@users.noreply.github.com>
2020-12-27 09:19:18 -0500

    Merge BiBOunded and UIComponent
    Merge BiBounded and UIComponent, reversing an earlier decision. This
    change removes the BiBounded class, which is not used by any other
    class and will probably never be.

--- src/main/java/com/autumnara/aikaterine/UIComponent.java
+++ src/main/java/com/autumnara/aikaterine/UIComponent.java
@@ -3,8 +3,27 @@ package com.autumnara.aikaterine;
 /** A component that can be drawn.
     By default this component does nothing and must be extended for any
     functionality. */
-public abstract class UIComponent extends BiBounded
+public abstract class UIComponent extends Bounded
 {
+
+    /** The boundaries of this component. */
+    protected WindowBoundary boundary;
+
+    /** Set spatial boundaries.
+        This method will ensure the object has been initialized, then
+        call {@link #onBounded} after setting the drawing area.
+        @param boundary the new boundary. */
+    public final void bound(WindowBoundary boundary)
+    {
+        this.assertInitialized();
+        this.boundary = boundary;
+        this.onBounded();
+    }
+
+    /** Update this object after spatial boundaries have been set.
+        By default this method does nothing and can be overrided by
+        subclasses. */
+    protected abstract void onBounded();
 
     /** Draw this component with it's present configuration.
         This method checks that this component is initialized and then
