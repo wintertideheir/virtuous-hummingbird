45eb3a01597d75c4f50f6092d729164d79d993ec
Sagun Singh <wintertideheir@users.noreply.github.com>
2021-01-03 12:26:26 -0500

    Integrate Viewport into AbstractView
    Add a Viewport member to AbstractView. AbstractView will now call
    Viewport.activate() before drawing.
    
    Add protected getters for AbstractView.viewport.width and
    AbstractView.viewport.height.

--- src/main/java/com/autumnara/aikaterine/AbstractView.java
+++ src/main/java/com/autumnara/aikaterine/AbstractView.java
@@ -3,12 +3,42 @@ package com.autumnara.aikaterine;
 public abstract class AbstractView extends AbstractResource
 {
 
+    private Viewport viewport;
+
+    public AbstractView(Viewport viewport)
+    {
+        this.setViewport(viewport);
+    }
+
     public final void render()
     {
         this.assertInitialized();
+        this.viewport.activate();
         this.onRender();
     }
 
     protected abstract void onRender();
+
+    public final void setViewport(Viewport viewport)
+    {
+        if (viewport.isRoot)
+        {
+            throw new IllegalArgumentException("View cannot be initialized with a viewport belonging to a window.");
+        }
+
+        this.viewport = viewport;
+
+        this.onSetViewport();
+    }
+
+    protected final int getViewportWidth()
+    {
+        return this.viewport.width;
+    }
+
+    protected final int getViewportHeight()
+    {
+        return this.viewport.height;
+    }
 
 }
