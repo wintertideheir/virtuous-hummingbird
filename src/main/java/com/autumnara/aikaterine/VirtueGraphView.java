a6c6c83aa813a88fbb29b6fe4f64c5725d80a3ba
Sagun Singh <wintertideheir@users.noreply.github.com>
2021-01-05 12:40:57 -0500

    Add a VirtueGraphView prototype
    Add a prototype view to render VirtueGraphs.

---
+++ src/main/java/com/autumnara/aikaterine/VirtueGraphView.java
@@ -0,0 +1,78 @@
+package com.autumnara.aikaterine;
+
+/** A view of a virtue graph.
+  *
+  * TODO: Complete this class.
+  */
+public final class VirtueGraphView extends AbstractView
+{
+
+    private final VirtueGraph graph;
+
+    private final Position center = new Position(0f, 0f);
+
+    private final float scale;
+
+    public final float scaleMinimum;
+
+    public final float scaleMaximum;
+
+    /** Constructor for a virtue graph view.
+      *
+      * @param viewportPromise the viewport promise for the
+      *                        {@link AbstractView} constructor
+      * @param graph           the virtue graph to be rendered
+      * @param scaleMinimum    the minimum view scale
+      * @param scale           the inital view scale. The view scale is
+      *                        the ratio of the radius of drawn virtue
+      *                        node (defined as 1) to the shortest
+      *                        dimension of this view's viewport. This
+      *                        value must always be positive.
+      * @param scaleMaximum    the maximum view scale
+      */
+    public VirtueGraphView(Promise<Viewport> viewportPromise,
+                           VirtueGraph       graph,
+                           float             scaleMinimum,
+                           float             scale,
+                           float             scaleMaximum)
+    {
+        super(viewportPromise);
+
+        if (scaleMinimum > scaleMaximum)
+        {
+            throw new IllegalArgumentException("Virtue graph view scale minimum must be less than or equal to the scale maximum.");
+        }
+        if (scale > scaleMaximum)
+        {
+            throw new IllegalArgumentException("Virtue graph view initial scale must be less than or equal to the scale maximum.");
+        }
+        if (scaleMinimum > scaleMaximum)
+        {
+            throw new IllegalArgumentException("Virtue graph view initial scale must be greater than or equal to the scale minimum.");
+        }
+        if ((scaleMinimum <= 0) ||
+            (scale        <= 0) ||
+            (scaleMaximum <= 0))
+        {
+            throw new IllegalArgumentException("Virtue graph view scale minimum, initial scale, and scale maximum must be positive. ");
+        }
+
+        this.graph        = graph;
+        this.scaleMinimum = scaleMinimum;
+        this.scale        = scale;
+        this.scaleMaximum = scaleMaximum;
+    }
+
+    @Override
+    protected void onInitialize() {}
+
+    @Override
+    protected void onTerminate() {}
+
+    @Override
+    protected void onRender() {}
+
+    @Override
+    protected void onSetViewport() {}
+
+}
