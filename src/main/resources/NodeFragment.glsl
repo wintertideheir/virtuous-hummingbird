8ad2178d9b71c5a29d41dbf73534ec1e7e9da930
Sagun Singh <wintertideheir@users.noreply.github.com>
2021-01-07 10:00:25 -0500

    Write node shaders

--- src/main/resources/NodeFragment.glsl
+++ src/main/resources/NodeFragment.glsl
@@ -1,8 +1,31 @@
+// Fragment shader for the nodes of a graph
+
 #version 330 core
 
-out vec4 color;
+in vec2 frag_mesh; // The node mesh passed from the vertex shader
+
+out vec4 color; // The final color
+
+uniform vec3 color; // The color of the node expressed as red-green-blue values
+
+const float fill_limit = 0.85;   // The maximum distance between the center and where the node is filled with color
+const float border_limit = 0.95; // The maximum distance between the center and where the node border ends
+const vec3 border_color = vec3(0.0f, 0.0f, 0.0f, 1.0f); // The color of the border (e.g. black)
+const vec3 empty_color = vec3(0.0f, 0.0f, 0.0f, 0.0f); // The color of the area beyond the border (e.g. transparent "black")
 
 void main()
 {
-    color = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+    mesh_distance = distance(frag_mesh, vec2(0f, 0f)); // Calculate the distance from a mesh point to it's center
+    if (mesh_distance < fill_limit)
+    {
+        color = vec4(color, 1.0f);
+    }
+    else if (mesh_distance < border_limit)
+    {
+        color = vec4(border_color, 1.0f);
+    }
+    else
+    {
+        color = vec4(empty_color, 1.0f);
+    }
 }
--- src/main/resources/NodeVertex.glsl
+++ src/main/resources/NodeVertex.glsl
@@ -1,8 +1,16 @@
+// Vertex shader for the nodes of a graph
+
 #version 330 core
 
-layout (location = 0) in vec2 pos;
+layout (location = 0) in vec2 mesh;
+
+out vec2 frag_mesh; // Pass the mesh to the fragment shader
+
+uniform vec2 scale;  // The scale factor for the x- and y-coordinates
+uniform vec2 offset; // The x- and y-offset of the mesh relative to the center of the drawing area
 
 void main()
 {
+    vec2 pos = (mesh * scale) + offset;
     gl_Position = vec4(pos.x, pos.y, 0.0, 1.0);
 }
