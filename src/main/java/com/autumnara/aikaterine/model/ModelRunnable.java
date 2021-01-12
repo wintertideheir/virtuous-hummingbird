a89289be2b58d803d11fd43860f38ca42adab49d
Sagun Singh <wintertideheir@users.noreply.github.com>
2021-01-12 06:45:40 -0500

    Document ModelRunnable and UIRunnable

--- src/main/java/com/autumnara/aikaterine/model/ModelRunnable.java
+++ src/main/java/com/autumnara/aikaterine/model/ModelRunnable.java
@@ -2,15 +2,35 @@ package com.autumnara.aikaterine.model;
 
 import java.util.concurrent.BlockingQueue;
 
+/** A runnable to manage the model.
+  *
+  * <p> The model-view-presenter pattern divides a program with a UI
+  * into three components based on seperation of concerns. These
+  * components can be seperated by language-level safety constructs
+  * like packages, and they can be seperated into threads. The latter
+  * allows for each component to independently favor throughput or
+  * latency.
+  *
+  * <p> This runnable begins a model manager when it's {@link #run}
+  * method is called. The model favors the high throughput required to
+  * perform certain calculations.
+  */
 public final class ModelRunnable
 implements Runnable
 {
 
+    /** Message queue for messages sent from the UI.
+      */
     private final BlockingQueue<Object> fromUI;
 
+    /** Message queue for messages sent to the UI.
+      */
     private final BlockingQueue<Object> toUI;
 
-    /** Constructor for a runnable execution thread  
+    /** Constructor for a runnable execution thread.
+      *
+      * @param fromUI message queue for messages sent from the UI
+      * @param toUI   message queue for messages sent to the UI
       */
     public ModelRunnable(BlockingQueue<Object> fromUI,
                          BlockingQueue<Object> toUI)
--- src/main/java/com/autumnara/aikaterine/ui/UIRunnable.java
+++ src/main/java/com/autumnara/aikaterine/ui/UIRunnable.java
@@ -2,15 +2,35 @@ package com.autumnara.aikaterine.ui;
 
 import java.util.concurrent.BlockingQueue;
 
+/** A runnable to manage the model.
+  *
+  * <p> The model-view-presenter pattern divides a program with a UI
+  * into three components based on seperation of concerns. These
+  * components can be seperated by language-level safety constructs
+  * like packages, and they can be seperated into threads. The latter
+  * allows for each component to independently favor throughput or
+  * latency.
+  *
+  * <p> This runnable begins a UI manager when it's {@link #run} method
+  * is called. The UI consists of the presenter and view. The UI
+  * requires a low latency for responsiveness.
+  */
 public final class UIRunnable
 implements Runnable
 {
 
+    /** Message queue for messages sent from the model.
+      */
     private final BlockingQueue<Object> fromModel;
 
+    /** Message queue for messages sent to the model.
+      */
     private final BlockingQueue<Object> toModel;
 
-    /** Constructor for a runnable execution thread  
+    /** Constructor for a runnable execution thread.
+      *
+      * @param fromModel message queue for messages sent from the model
+      * @param toModel   message queue for messages sent to the model
       */
     public UIRunnable(BlockingQueue<Object> fromModel,
                       BlockingQueue<Object> toModel)
